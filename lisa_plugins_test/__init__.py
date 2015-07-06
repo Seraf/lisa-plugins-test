d919496d0f291d4aa5861ed6e83de3701c0f2419
Julien Syx <julien.syx@gmail.com>
2015-07-06 13:27:56 +0200

    Initial test commit

---
+++ lisa_plugins_test/__init__.py
@@ -0,0 +1,25 @@
+from lisa_api.lisa.plugin import PluginBase
+
+
+class TestPlugin(PluginBase):
+    def __init__(self):
+        pass
+
+    def add_intents(self):
+        from lisa_api.api.models import Intent
+
+
+__title__ = 'Lisa Plugins Test'
+__version__ = '0.1.0'
+__author__ = 'Julien Syx'
+__license__ = 'Apache'
+__copyright__ = 'Copyright 2015 Julien Syx'
+
+# Version synonym
+VERSION = __version__
+
+# Header encoding (see RFC5987)
+HTTP_HEADER_ENCODING = 'iso-8859-1'
+
+# Default datetime input and output formats
+ISO_8601 = 'iso-8601'
---
+++ tests/v1/test_example.py
@@ -0,0 +1,7 @@
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+
+class ExampleTests(APITestCase):
+    def setUp(self):
+        pass
